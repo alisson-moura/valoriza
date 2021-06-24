f02e52f1df773bf3d4205a6da49fda26bed68412
alisson-moura <alisson.mo.moura@outlook.com.br>
2021-06-23 22:30:42 -0300

    corrigido retorno do método

--- src/services/Tags/CreateTagService.ts
+++ src/services/Tags/CreateTagService.ts
@@ -1,10 +1,11 @@
 import { getCustomRepository } from 'typeorm'
+import { Tag } from '../../entities/Tag'
 import { ErrorProvider } from '../../providers/ErrorProvider'
 import { TagsRepository } from '../../repositories/TagsRepository'
 import { I_TagDTO } from './TagDTO'
 
 class CreateTagService {
-  async execute({ name, description }: I_TagDTO) {
+  async execute({ name, description }: I_TagDTO): Promise<Tag> {
     const tagsRepository = getCustomRepository(TagsRepository)
 
     const tagAlreadyExists = await tagsRepository.findOne({ name })
