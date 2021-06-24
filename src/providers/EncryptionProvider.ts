5cb1a0ddd75a096e84a4a5a85d3d498eef58c689
alisson-moura <alisson.mo.moura@outlook.com.br>
2021-06-23 21:21:34 -0300

    renomeado classe

--- src/providers/EncryptionProvider.ts
+++ src/providers/EncryptionProvider.ts
@@ -1,12 +1,12 @@
 import { hash, compare } from 'bcryptjs'
 
-class EncryptProvider {
+class EncryptionProvider {
   async encrypt(value: string): Promise<string> {
     return hash(value, 8)
   }
 
-  async compare(value: string, encryptedValue: string) {
+  async compare(value: string, encryptedValue: string): Promise<boolean> {
     return compare(value, encryptedValue)
   }
 }
-export { EncryptProvider }
+export { EncryptionProvider }
