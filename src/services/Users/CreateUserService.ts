f56c5868d6e0cdf95b8b34d2b7e8790d16f4a733
alisson-moura <alisson.mo.moura@outlook.com.br>
2021-06-23 21:20:54 -0300

    Corrigido import

--- src/services/Users/CreateUserService.ts
+++ src/services/Users/CreateUserService.ts
@@ -1,6 +1,6 @@
 import { getCustomRepository } from 'typeorm'
 import { ErrorProvider } from '../../providers/ErrorProvider'
-import { EncryptProvider } from '../../providers/EncryptionProvider'
+import { EncryptionProvider } from '../../providers/EncryptionProvider'
 import { UsersRepository } from '../../repositories/UsersRepository'
 import { I_UserDTO } from './UserDTO'
 
@@ -13,7 +13,7 @@ class CreateUserService {
       throw new ErrorProvider("User already exists", 409)
     }
 
-    const encryptionProvider = new EncryptProvider()
+    const encryptionProvider = new EncryptionProvider()
     const passwordEncrypted = await encryptionProvider.encrypt(password)
 
     const user = usersRepository.create({
