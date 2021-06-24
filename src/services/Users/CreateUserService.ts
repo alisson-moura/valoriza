d43f48ef95b8846a5541251c87fdf632f9665c04
alisson-moura <alisson.mo.moura@outlook.com.br>
2021-06-23 22:29:47 -0300

    corrigido valor default da propriedade admin

--- src/services/Users/CreateUserService.ts
+++ src/services/Users/CreateUserService.ts
@@ -5,7 +5,7 @@ import { UsersRepository } from '../../repositories/UsersRepository'
 import { I_UserDTO } from './UserDTO'
 
 class CreateUserService {
-  async execute({ email, name, password, admin }: I_UserDTO) {
+  async execute({ email, name, password, admin = false }: I_UserDTO) {
     const usersRepository = getCustomRepository(UsersRepository)
 
     const userAlreadyExists = await usersRepository.findOne({ email })
@@ -20,7 +20,7 @@ class CreateUserService {
       name,
       password: passwordEncrypted,
       email,
-      admin: admin == undefined ? false : admin
+      admin
     })
     await usersRepository.save(user)
 
