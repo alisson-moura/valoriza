c5f720e1ad09dd49fed6d44aa1cd9692765d6c0d
alisson-moura <alisson.mo.moura@outlook.com.br>
2021-06-23 22:32:02 -0300

    adicionado rota para criar compliments

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express'
+
 import TagsController from './controllers/TagsController'
 import UsersController from './controllers/UsersController'
+import ComplimentsController from './controllers/ComplimentsController'
+
 import { ensureAdmin } from './middlewares/ensureAdmin'
 
 const router = Router()
 
 const usersController = new UsersController()
 const tagsController = new TagsController()
+const complimentsController = new ComplimentsController()
 
 // users routes
 router.post('/users', usersController.store)
@@ -14,5 +18,8 @@ router.post('/auth', usersController.authenticate)
 
 // tags routes
 router.post('/tags', ensureAdmin, tagsController.store)
+
+// compliments routes
+router.post('/compliments', ensureAdmin, complimentsController.store)
 
 export { router }
