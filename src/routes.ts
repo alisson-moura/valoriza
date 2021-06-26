8bae0c90e9e1dca3958cc398ba5dbfac866300a5
alisson-moura <alisson.mo.moura@outlook.com.br>
2021-06-25 23:04:42 -0300

    adicionado rotas para listar recursos

--- src/routes.ts
+++ src/routes.ts
@@ -16,12 +16,16 @@ const complimentsController = new ComplimentsController()
 // users routes
 router.post('/users', usersController.store)
 router.post('/auth', usersController.authenticate)
+router.get('/users', ensureAuthenticated, usersController.index)
+router.get('/users/:id', ensureAuthenticated, usersController.show)
 
 // tags routes
 router.post('/tags', ensureAuthenticated, ensureAdmin, tagsController.store)
+router.get('/tags', ensureAuthenticated, ensureAdmin, tagsController.index)
 
 // compliments routes
 router.post('/compliments', ensureAuthenticated, complimentsController.store)
+router.get('/compliments/:id', ensureAuthenticated, complimentsController.index)
 
 router.get('/', ensureAuthenticated, (request, response) => {
   return response.status(200).json({})
